81d0d22ea7ce6d7ee70d3fb0ef34f6116ac82f16
Josh Brito <jbrito6492@gmail.com>
2022-05-04 20:50:17 -0700

    Behavioral - added vonage_session_id as class attr on Meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,5 +2,6 @@ class Meeting < ApplicationRecord
   before create do
     opentok = OpenTok::OpenTok.new Rails.application.credentials.vonage_api.key,
                                    Rails.application.credentials.vonage_api.secret
+    self.vonage_session_id = opentok.create_session.session_id
   end
 end
