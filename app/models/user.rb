b92cd019e6de29b21c5156b8bd28b9296806ded6
Josh Brito <jbrito6492@gmail.com>
2022-05-25 20:46:05 -0700

    Behavioral - added first and last name validations and text fields on registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
-  has_person_name
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_person_name
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
